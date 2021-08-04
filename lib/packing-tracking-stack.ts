cec92d41144e3e5a0a98f942405f17ecd217587d
Stefan Feltmann <stefan.feltmann@gmail.com>
2021-08-03 22:54:38 -0700

    (WIP) Working on setting up CDK Stack

--- lib/packing-tracking-stack.ts
+++ lib/packing-tracking-stack.ts
@@ -1,9 +1,28 @@
 import * as cdk from '@aws-cdk/core';
+import rds = require('@aws-cdk/aws-rds')
+import ec2 = require('@aws-cdk/aws-ec2')
 
 export class PackingTrackingStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const vpc = new ec2.Vpc(this, 'QuakeAlertVPC', {
+      cidr: '10.0.0.0/16',
+    })
+
     // The code that defines your stack goes here
+    const dbInstance = new rds.DatabaseInstance(this, 'QuakeInstance', {
+      engine: rds.DatabaseInstanceEngine.postgres({ version: rds.PostgresEngineVersion.VER_13 }),
+      // optional, defaults to m5.large
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.MICRO),
+      credentials: rds.Credentials.fromGeneratedSecret('hasuraConfigSecret'), // Optional - will default to 'admin' username and generated password
+      vpc,
+      vpcSubnets: {
+        subnetType: ec2.SubnetType.PRIVATE,
+      },
+      deletionProtection: false,
+      deleteAutomatedBackups: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    })
   }
 }
---
+++ scripts/install-handler-deps.ts
@@ -0,0 +1,33 @@
+import fs = require('fs')
+import child_process = require('child_process')
+;(async () => {
+  const dirs = ['lib']
+  for (const dir of dirs) {
+    process.chdir(dir)
+    const items = fs.readdirSync('./')
+    for (const handlerDir of items) {
+      const handlerPath = `./${handlerDir}`
+      const stat = await fs.promises.lstat(handlerPath)
+      if (!stat.isDirectory()) {
+        continue
+      }
+      install(handlerPath, handlerDir)
+    }
+
+    process.chdir('..')
+    install(`./${dir}`, dir)
+  }
+
+  console.log('Done installing handler dependencies!')
+})()
+
+function install(handlerPath: string, handlerDir: string) {
+  const handlerItems = fs.readdirSync(handlerPath)
+  if (handlerItems.some((item) => item === 'package.json')) {
+    console.log(`Installing dependencies for ${handlerDir}...`)
+    process.chdir(handlerDir)
+    child_process.execSync('npm install')
+    child_process.execSync('npm ci')
+    process.chdir('..')
+  }
+}
