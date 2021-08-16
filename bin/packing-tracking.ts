3ef49e9b0121aeb479f1e7e0f79cdae70d9c412e
Stefan Feltmann <stefan.feltmann@gmail.com>
2021-08-15 23:29:51 -0700

    #11 (WIP) Breaking stack into multiples

--- bin/packing-tracking.ts
+++ bin/packing-tracking.ts
@@ -1,26 +1,61 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { PackingTrackingStack } from '../lib/packing-tracking-stack';
+import 'source-map-support/register'
+import * as cdk from '@aws-cdk/core'
+import { PackingTrackingCoreStack } from '../lib/packing-tracking-stack'
+import { PackingTrackingHasuraStack } from '../lib/hasura-stack'
 require('dotenv').config()
 
-const app = new cdk.App();
-new PackingTrackingStack(app, 'PackingTrackingStack', {
-  env: { 
-    account: process.env.CDK_DEFAULT_ACCOUNT, 
-    region: process.env.CDK_DEFAULT_REGION 
+interface EnvProps {
+  env: any
+  stage: string
 }
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+class PackingTrackingService extends cdk.Construct {
+  constructor(scope: cdk.Construct, id: string, props: EnvProps) {
+    super(scope, id)
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+    const multiAz = false
 
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+    const coreStack = new PackingTrackingCoreStack(scope, 'PackingTrackingCoreStack', {
+      env: props.env,
+      stage: props.stage,
+      multiAz,
+    })
+
+    // type HasuraStackProps = {
+    //   stage: string
+    //   multiAz: boolean
+    //   vpc: Vpc
+    //   appName: string
+    //   credentials: Credentials
+    //   databaseInstance: DatabaseInstance
+    //   certificate: DnsValidatedCertificate
+    //   subdomain: string
+    //   rootDomain: string
+    //   hasuraGraphqlAdminSecret: Secret
+    // } & StackProps
+
+    const hasuraStack = new PackingTrackingHasuraStack(scope, 'PackingTrackingHasuraStack', {
+      env: props.env,
+      stage: props.stage,
+      multiAz,
+      vpc: coreStack.vpc,
+      appName: 'PackingTracking',
+      credentials: coreStack.credentials,
+      databaseInstance: coreStack.databaseInstance,
+      certificate: coreStack.certificate,
+      subdomain: coreStack.subdomain,
+      rootDomain: coreStack.rootDomain,
+      hasuraGraphqlAdminSecret: coreStack.hasuraGraphqlAdminSecret,
+    })
+  }
+}
+
+const app = new cdk.App()
+new PackingTrackingService(app, 'prod', {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
+  stage: 'prod',
+})
--- test/packing-tracking.test.ts
+++ test/packing-tracking.test.ts
@@ -1,13 +1,18 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
-import * as cdk from '@aws-cdk/core';
-import * as PackingTracking from '../lib/packing-tracking-stack';
+import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert'
+import * as cdk from '@aws-cdk/core'
+import * as PackingTracking from '../lib/packing-tracking-stack'
 
 test('Empty Stack', () => {
-    const app = new cdk.App();
-    // WHEN
-    const stack = new PackingTracking.PackingTrackingStack(app, 'MyTestStack');
-    // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
-});
+  const app = new cdk.App()
+  // WHEN
+  const stack = new PackingTracking.PackingTrackingCoreStack(app, 'MyTestStack')
+  // THEN
+  expectCDK(stack).to(
+    matchTemplate(
+      {
+        Resources: {},
+      },
+      MatchStyle.EXACT
+    )
+  )
+})
